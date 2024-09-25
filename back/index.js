48cd64cd69ca73e7f9620f6cc32886379746a459
Dhruvi Shah <shahdhruvi5081@gmail.com>
2024-09-25 20:08:39 +0530

    Fix

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const summarizeText = require('./summarize.js');
 app.use(cors());
 // Parses JSON bodies (as sent by API clients)
